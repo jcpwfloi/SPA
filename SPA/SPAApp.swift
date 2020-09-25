b710e46152c81a81f8c665731ef65f98a55d633d
Team F <jcpwfloi@live.unc.edu,leochanj@live.unc.edu,bokai247@live.unc.edu>
2020-09-24 20:16:10 -0400

    initial commit

---
+++ SPA/SPAApp.swift
@@ -0,0 +1,20 @@
+//
+//  SPAApp.swift
+//  SPA
+//
+//  Created by Zhongrui Chen on 2020-09-24.
+//
+
+import SwiftUI
+
+@main
+struct SPAApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
