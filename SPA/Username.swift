2f76ec7bb4445a5c58ac209d133d1e4a9e0813b3
Bokai Li <bokai247@gmail.com>
2020-10-07 20:02:20 -0400

    user list view #3

---
+++ SPA/Username.swift
@@ -0,0 +1,15 @@
+//
+//  username.swift
+//  SPA
+//
+//  Created by Bokai Li on 10/7/20.
+//
+
+import Foundation
+
+class Username{
+    var title:String
+    init(title:String){
+        self.title=title
+    }
+}
