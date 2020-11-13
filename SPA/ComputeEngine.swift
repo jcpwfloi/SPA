4f3c50d9d844e402f819fe0f20e5f0ee4f8aa3ae
Jingyuan Chen <leochan@jcpwfloi.wireless-1x.unc.edu>
2020-11-12 22:19:29 -0500

    leochan wiop

--- SPA/ComputeEngine.swift
+++ SPA/ComputeEngine.swift
@@ -2239,58 +2239,58 @@ func computeMetrics() -> [(String, Double?, String?)]? {
 
 
 
-func computeMetrics(_ outputValues: [(String, Double?, String?)]) -> [String : (Double?, String?)] {
+func computeMetrics(_ outputValues: [(String, Double?, String?)]) -> [String : String] {
     
-    var ans = [String: (Double?, String?)]()
+    var ans = [String: String]()
     
     for tuple in outputValues {
         
         switch "\(tuple.0)" {
             
         case "OutputIndex00":
-            ans["ProjectId"] = (nil, tuple.2)
+            ans["ProjectId"] = tuple.2!
             
         case "OutputIndex01":
-            ans["Project Productivity"] = (tuple.1, nil)
+            ans["Project Productivity"] = String(format:"%1.0f", tuple.1!)
             
         case "OutputIndex02":
-            ans["Software Quality"] = (tuple.1, nil)
+            ans["Software Quality"] = String(format:"%1.0f", tuple.1!)
             
         case "OutputIndex03":
-            ans["Project Cost Effectiveness"] = (tuple.1, nil)
+            ans["Project Cost Effectiveness"] = String(format:"%1.0f", tuple.1!)
             
         case "OutputIndex04":
-            ans["Development Process Effectiveness"] = (tuple.1, nil)
+            ans["Development Process Effectiveness"] = String(format:"%1.0f", tuple.1!)
             
         case "OutputIndex04a":
-            ans["Est Project Failure Risk"] = (tuple.1, nil)
+            ans["Est Project Failure Risk"] = String(format:"%3.0f", tuple.1!) + "%"
             
         case "OutputIndex04b":
-            ans["Est Project Delay Risk"] = (tuple.1, nil)
+            ans["Est Project Delay Risk"] = String(format:"%3.0f", tuple.1!) + "%"
             
         case "OutputIndex04c":
-            ans["Est Software Poor Quality Risk"] = (tuple.1, nil)
-            
+            ans["Est Software Poor Quality Risk"] = String(format:"%3.0f", tuple.1!) + "%"
+
         case "OutputIndex4d":
-            ans["Est Complete Early"] = (tuple.1, nil)
+            ans["Est Complete Early"] = String(format:"%3.0f", tuple.1!) + "%"
             
         case "OutputIndex4e":
-            ans["Est Complete On-Time"] = (tuple.1, nil)
+            ans["Est Complete On-Time"] = String(format:"%3.0f", tuple.1!) + "%"
             
         case "OutputIndex4f":
-            ans["Est Complete Delayed"] = (tuple.1, nil)
+            ans["Est Complete Delayed"] = String(format:"%3.0f", tuple.1!) + "%"
             
         case "OutputIndex4g":
-            ans["Est Complete Canceled"] = (tuple.1, nil)
+            ans["Est Complete Canceled"] = String(format:"%3.0f", tuple.1!) + "%"
             
         case "OutputIndex4h":
-            ans["Est Cost of Quality"] = (tuple.1, nil)
+            ans["Est Cost of Quality"] = String(format:"%3.0f", tuple.1!) + "%"
             
         case "OutputIndex4i":
-            ans["Est Total Project Cost"] = (tuple.1, nil)
+            ans["Est Total Project Cost"] = tuple.1!.toMoney(toLength: 10)
             
         case "OutputIndex4j":
-            ans["Est Project Duration"] = (tuple.1, nil)
+            ans["Est Project Duration"] = String(format:"%3.1f", tuple.1!) + " Ems"
             
             
         case "OutputIndex05":
@@ -2301,202 +2301,202 @@ func computeMetrics(_ outputValues: [(String, Double?, String?)]) -> [String : (
             print("     \(tuple.2!)", terminator: "")
             
         case "OutputIndex06":
-            ans["Project Team Size"] = (tuple.1, nil)
+            ans["Project Team Size"] = String(format:"%4.0f", tuple.1!)
             
         case "OutputIndex07":
-            ans["Labor Rate"] = (tuple.1, nil)
+            ans["Labor Rate"] = "$" + String(format:"%3.0f", tuple.1!)
             
         case "OutputIndex08":
-            ans["Total Project Hours"] = (tuple.1, nil)
+            ans["Total Project Hours"] = String(format:"%11.0f", tuple.1!) + " Ehrs"
             
         case "OutputIndex08a":
-            ans["Development Effort Percent"] = (tuple.1, nil)
+            ans["Development Effort Percent"] = String(format:"%3.0f", tuple.1!) + "%"
             
         case "OutputIndex08b":
-            ans["Test Effort Percent"] = (tuple.1, nil)
+            ans["Test Effort Percent"] = String(format:"%3.0f", tuple.1!) + "%"
             
         case "OutputIndex09":
-            ans["Programming Language"] = (nil, tuple.2)
+            ans["Programming Language"] = tuple.2!
             
         case "OutputIndex09a":
-            ans["Programming Language Level"] = (tuple.1, nil)
+            ans["Programming Language Level"] = String(format:"%2.0f", tuple.1!)
             
         case "OutputIndex10":
-            ans["NumNcnbSloc"] = (tuple.1, nil)
+            ans["NumNcnbSloc"] = String(format:"%9.0f", tuple.1!)
             
         case "OutputIndex10a":
-            ans["Number of Issues"] = (tuple.1, nil)
+            ans["Number of Issues"] = String(format:"%6.0f", tuple.1!)
             
         case "OutputIndex10b":
-            ans["Est Number of Severity 1 Issues"] = (tuple.1, nil)
+            ans["Est Number of Severity 1 Issues"] = String(format:"%6.0f", tuple.1!)
             
         case "OutputIndex10c":
-            ans["Est Number of Severity 2 Issues"] = (tuple.1, nil)
+            ans["Est Number of Severity 2 Issues"] = String(format:"%6.0f", tuple.1!)
             
         case "OutputIndex10d":
-            ans["Est Number of Severity 3 Issues"] = (tuple.1, nil)
+            ans["Est Number of Severity 3 Issues"] = String(format:"%6.0f", tuple.1!)
             
         case "OutputIndex10e":
-            ans["Est Number of Severity 4 Issues"] = (tuple.1, nil)
+            ans["Est Number of Severity 4 Issues"] = String(format:"%6.0f", tuple.1!)
             
         case "OutputIndex11":
-            ans["CMMI Maturity Level"] = (tuple.1, nil)
+            ans["CMMI Maturity Level"] = String(format:"%1.0f", tuple.1!)
             
         case "OutputIndex12":
-            ans["Programming Productivity"] = (tuple.1, nil)
+            ans["Programming Productivity"] = String(format:"%4.0f", tuple.1!) + " NcSloc per SE per Em"
             
         case "OutputIndex13":
-            ans["Est PreRelease Defects"] = (tuple.1, nil)
+            ans["Est PreRelease Defects"] = String(format:"%4.0f", tuple.1!) + " Dfs"
             
         case "OutputIndex14":
-            ans["Est PostRelease Defects"] = (tuple.1, nil)
+            ans["Est PostRelease Defects"] = String(format:"%4.0f", tuple.1!) + " Dfs"
             
         case "OutputIndex140a":
-            ans["Est Expected Delivered Defects"] = (tuple.1, nil)
+            ans["Est Expected Delivered Defects"] = String(format:"%4.0f", tuple.1!) + " Dfs"
             
         case "OutputIndex14a":
-            ans["Est PreRelease Requirements Defects"] = (tuple.1, nil)
+            ans["Est PreRelease Requirements Defects"] = String(format:"%4.0f", tuple.1!) + " Dfs"
             
         case "OutputIndex14b":
-            ans["Est PreRelease Design Defects"] = (tuple.1, nil)
+            ans["Est PreRelease Design Defects"] = String(format:"%4.0f", tuple.1!) + " Dfs"
             
         case "OutputIndex14c":
-            ans["Est PreRelease Code Defects"] = (tuple.1, nil)
+            ans["Est PreRelease Code Defects"] = String(format:"%4.0f", tuple.1!) + " Dfs"
             
         case "OutputIndex14d":
-            ans["Est PreRelease Test Defects"] = (tuple.1, nil)
+            ans["Est PreRelease Test Defects"] = String(format:"%4.0f", tuple.1!) + " Dfs"
             
         case "OutputIndex14e":
-            ans["Est PreRelease Documentation Defects"] = (tuple.1, nil)
+            ans["Est PreRelease Documentation Defects"] = String(format:"%4.0f", tuple.1!) + " Dfs"
             
         case "OutputIndex14f":
-            ans["Est PreRelease Bad-Fix Defects"] = (tuple.1, nil)
+            ans["Est PreRelease Bad-Fix Defects"] = String(format:"%4.0f", tuple.1!) + " Dfs"
             
         case "OutputIndex15":
-            ans["Software Defect Density"] = (tuple.1, nil)
+            ans["Software Defect Density"] = String(format:"%3.0f", tuple.1!) + " Dfs / KncSloc"
             
         case "OutputIndex15a":
-            ans["Industry Average Defect Density"] = (tuple.1, nil)
+            ans["Industry Average Defect Density"] = String(format:"%3.0f", tuple.1!) + " Dfs / KncSloc"
             
         case "OutputIndex16":
-            ans["PotentialDfsLeft"] = (tuple.1, nil)
+            ans["PotentialDfsLeft"] = String(format:"%5.0f", tuple.1!) + " Dfs"
             
         case "OutputIndex16a":
-            ans["Est Requirement Defects Left"] = (tuple.1, nil)
+            ans["Est Requirement Defects Left"] = String(format:"%5.0f", tuple.1!) + " Dfs"
             
         case "OutputIndex16b":
-            ans["Est Design Defects Left"] = (tuple.1, nil)
+            ans["Est Design Defects Left"] = String(format:"%5.0f", tuple.1!) + " Dfs"
             
         case "OutputIndex16c":
-            ans["Est Code Defects Left"] = (tuple.1, nil)
+            ans["Est Code Defects Left"] = String(format:"%5.0f", tuple.1!) + " Dfs"
             
         case "OutputIndex16d":
-            ans["Est Security Defects Left"] = (tuple.1, nil)
+            ans["Est Security Defects Left"] = String(format:"%5.0f", tuple.1!) + " Dfs"
             
         case "OutputIndex16e":
-            ans["Est User Documentation Defects Left"] = (tuple.1, nil)
+            ans["Est User Documentation Defects Left"] = String(format:"%5.0f", tuple.1!) + " Dfs"
             
         case "OutputIndex16f":
-            ans["Est Test Defects Left"] = (tuple.1, nil)
+            ans["Est Test Defects Left"] = String(format:"%5.0f", tuple.1!) + " Dfs"
             
         case "OutputIndex16g":
-            ans["Est Bad-Fix Defects Left"] = (tuple.1, nil)
+            ans["Est Bad-Fix Defects Left"] = String(format:"%5.0f", tuple.1!) + " Dfs"
             
         case "OutputIndex17":
-            ans["Est Defects per Engineering Month"] = (tuple.1, nil)
+            ans["Est Defects per Engineering Month"] = String(format:"%4.0f", tuple.1!) + " Dfs per Em"
             
         case "OutputIndex18":
-            ans["Est Software Inspection Productivity"] = (tuple.1, nil)
+            ans["Est Software Inspection Productivity"] = String(format:"%4.0f", tuple.1!) + " Dfs per Em"
             
         case "OutputIndex19":
-            ans["Est Quality Assurance Productivity"] = (tuple.1, nil)
+            ans["Est Quality Assurance Productivity"] = String(format:"%4.0f", tuple.1!) + " Dfs per Em"
             
         case "OutputIndex20":
-            ans["Est Defects Leaked to Users"] = (tuple.1, nil)
+            ans["Est Defects Leaked to Users"] = String(format:"%4.0f", tuple.1!) + " Dfs per Em"
             
         case "OutputIndex21":
-            ans["Est Project Defect Removal Efficiency"] = (tuple.1, nil)
+            ans["Est Project Defect Removal Efficiency"] = String(format:"%3.0f", tuple.1!) + "%"
             
         case "OutputIndex22":
-            ans["Est Capability-Based DRE"] = (nil, tuple.2)
+            ans["Est Capability-Based DRE"] = tuple.2!
             
         case "OutputIndex22a":
-            ans["Est US Average DRE"] = (tuple.1, nil)
+            ans["Est US Average DRE"] = String(format:"%3.0f", tuple.1!) + "%"
             
         case "OutputIndex22b":
-            ans["Est US Average DDE"] = (tuple.1, nil)
+            ans["Est US Average DDE"] = String(format:"%3.0f", tuple.1!) + "%"
             
         case "OutputIndex23":
-            ans["Est Find Fix Effort Per Defect"] = (tuple.1, nil)
+            ans["Est Find Fix Effort Per Defect"] = String(format:"%3.1f", tuple.1!) + " Ehs per Dfs"
             
         case "OutputIndex24":
-            ans["Est Avg Find Fix Effort per Defect"] = (tuple.1, nil)
+            ans["Est Avg Find Fix Effort per Defect"] = String(format:"%3.1f", tuple.1!) + " Ehs per Dfs"
             
         case "OutputIndex25":
-            ans["Est Unit Cost per Engineering Month"] = (tuple.1, nil)
+            ans["Est Unit Cost per Engineering Month"] = "$" + String(format:"%3.1f", tuple.1!) + " per NcSloc per Em"
             
         case "OutputIndex28":
-            ans["Est Find Fix Percentage"] = (tuple.1, nil)
+            ans["Est Find Fix Percentage"] = String(format:"%3.0f", tuple.1!) + "%"
             
         case "OutputIndex28a":
-            ans["Est Project Effort"] = (tuple.1, nil)
+            ans["Est Project Effort"] = String(format:"%6.0f", tuple.1!) + " Staff-Em"
             
         case "OutputIndex28b":
-            ans["Est Requirements Effort"] = (tuple.1, nil)
+            ans["Est Requirements Effort"] = String(format:"%6.0f", tuple.1!) + " Staff-Em"
             
         case "OutputIndex28c":
-            ans["Est Design Effort"] = (tuple.1, nil)
+            ans["Est Design Effort"] = String(format:"%6.0f", tuple.1!) + " Staff-Em"
             
         case "OutputIndex28d":
-            ans["Est Code Effort"] = (tuple.1, nil)
+            ans["Est Code Effort"] = String(format:"%6.0f", tuple.1!) + " Staff-Em"
             
         case "OutputIndex28e":
-            ans["Est Test Effort"] = (tuple.1, nil)
+            ans["Est Test Effort"] = String(format:"%6.0f", tuple.1!) + " Staff-Em"
             
         case "OutputIndex28f":
-            ans["Est Total Project Staffing"] = (tuple.1, nil)
+            ans["Est Total Project Staffing"] = String(format:"%3.0f", tuple.1!) + " Staff-Members"
             
         case "OutputIndex28g":
-            ans["Est Requirements Staffing"] = (tuple.1, nil)
+            ans["Est Requirements Staffing"] = String(format:"%3.0f", tuple.1!) + " Staff-Members"
             
         case "OutputIndex28h":
-            ans["Est Design Staffing"] = (tuple.1, nil)
+            ans["Est Design Staffing"] = String(format:"%3.0f", tuple.1!) + " Staff-Members"
             
         case "OutputIndex28i":
-            ans["Est Coding Staffing"] = (tuple.1, nil)
+            ans["Est Coding Staffing"] = String(format:"%3.0f", tuple.1!) + " Staff-Members"
             
         case "OutputIndex28j":
-            ans["Est Test Staffing"] = (tuple.1, nil)
+            ans["Est Test Staffing"] = String(format:"%3.0f", tuple.1!) + " Staff-Members"
             
         case "OutputIndex29":
-            ans["Est Project Duration"] = (tuple.1, nil)
+            ans["Est Project Duration"] = String(format:"%3.1f", tuple.1!) + " Ems"
             
         case "OutputIndex29a":
-            ans["Est Design, Code & Test Cost"] = (tuple.1, nil)
+            ans["Est Design, Code & Test Cost"] = "$" + String(format:"%11.0f", tuple.1!)
             
         case "OutputIndex29b":
-            ans["Est Team Cost per Engineering Month"] = (tuple.1, nil)
+            ans["Est Team Cost per Engineering Month"] = "$" + String(format:"%11.0f", tuple.1!) + " per Em"
             
         case "OutputIndex29c":
-            ans["Est Cost of Quality"] = (tuple.1, nil)
+            ans["Est Cost of Quality"] = "$" + String(format:"%11.0f", tuple.1!)
             
         case "OutputIndex29d":
-            ans["Est Technical Debt"] = (tuple.1, nil)
+            ans["Est Technical Debt"] = "$" + String(format:"%6.0f", tuple.1!)
             
         case "OutputIndex30":
-            ans["Est Total Development Cost"] = (tuple.1, nil)
+            ans["Est Total Development Cost"] = "$" + String(format:"%11.0f", tuple.1!)
             
         case "OutputIndex30a":
-            ans["Est Lifetime Maintenance Cost"] = (tuple.1, nil)
+            ans["Est Lifetime Maintenance Cost"] = "$" + String(format:"%11.0f", tuple.1!)
             
         case "OutputIndex30b":
-            ans["Est Total Project Cost"] = (tuple.1, nil)
+            ans["Est Total Project Cost"] = "$" + String(format:"%11.0f", tuple.1!)
             
         case "OutputIndex30c":
-            ans["Est Agile Invested Savings"] = (tuple.1, nil)
+            ans["Est Agile Invested Savings"] = "$" + String(format:"%9.0f", tuple.1!)
             
         case "OutputIndex31":
-            ans["Est Cost-Effectiveness Indicator"] = (tuple.1, nil)
+            ans["Est Cost-Effectiveness Indicator"] = String(format:"%1.1f", tuple.1!)
             
         default:
             print("Invalid Output Index", terminator: "")
@@ -2507,20 +2507,20 @@ func computeMetrics(_ outputValues: [(String, Double?, String?)]) -> [String : (
     
     return ans
 }
-
-func computeMetricsString(_ outputValues: [(String, Double?, String?)]) -> [String : String] {
-    let ans = computeMetrics(outputValues)
-    
-    var ret = [String : String] ()
-    
-    for (key, value) in ans {
-        if value.0 == nil {
-            ret[key] = value.1!
-        } else {
-            ret[key] = String(format: "%f", value.0!)
-        }
-    }
-    
-    return ret
-}
+//
+//func computeMetricsString(_ outputValues: [(String, Double?, String?)]) -> [String : String] {
+//    let ans = computeMetrics(outputValues)
+//
+//    var ret = [String : String] ()
+//
+//    for (key, value) in ans {
+//        if value.0 == nil {
+//            ret[key] = value.1!
+//        } else {
+//            ret[key] = String(format: "%f", value.0!)
+//        }
+//    }
+//
+//    return ret
+//}
 
